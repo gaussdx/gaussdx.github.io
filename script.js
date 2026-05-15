3497b1e765fa36400c169bef1709af6533456006
gaussdx <165459511+gaussdx@users.noreply.github.com>
2026-05-15 19:17:53 +0800

    Add visitor counter to footer

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ if (footerLink) {
   footerLink.href = "https://gaussdx.github.io/";
   footerLink.textContent = "https://gaussdx.github.io/";
 }
+
+const footer = document.querySelector(".site-footer");
+
+if (footer) {
+  const counter = document.createElement("span");
+  counter.id = "busuanzi_container_site_pv";
+  counter.className = "visitor-counter";
+  counter.style.display = "none";
+  counter.innerHTML = '访问量：<span id="busuanzi_value_site_pv"></span>';
+
+  const footerUrl = footer.querySelector("a");
+  if (footerUrl) {
+    footer.insertBefore(counter, footerUrl);
+  } else {
+    footer.appendChild(counter);
+  }
+
+  const busuanzi = document.createElement("script");
+  busuanzi.async = true;
+  busuanzi.src = "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+  document.body.appendChild(busuanzi);
+}
